3159b4f63760b2c799b82c7e6b69e5bd20c6d081
Ayan-Choudhury-dev <ayan26199+github@gmail.com>
2026-01-22 12:59:42 +0530

    Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,24 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.ayanchoudhury.in",
-  integrations: [mdx(), sitemap(), tailwind(), react({experimentalReactChildren:true,}), markdoc(), keystatic()],
+  integrations: [mdx(), sitemap(), tailwind(), react({ experimentalReactChildren: true, }), markdoc(), keystatic()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        limitInputPixels: false,
+      },
+    },
+    // We strictly want WebP for performance - AVIF is too slow to build
+    domains: [],
+    remotePatterns: [{ protocol: "https" }],
+  },
   output: "static",
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      formats: ['image/webp'], // Force Vercel to only optimize to WebP, skipping AVIF
+    },
+  }),
 });
